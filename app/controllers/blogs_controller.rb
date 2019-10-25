45e6e9349a181a6ba0919bbaa6de350aa49b7f04
sakitayu <sakitakitakita@gmail.com>
2019-10-25 17:21:13 +0900

    他人のプロフィールページを閲覧できる機能を追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,7 +16,7 @@ class BlogsController < ApplicationController
       render :new
     else
       if @blog.save
-        redirect_to blogs_path, notice: "ブログを作成しました！"
+        redirect_to blogs_path
       else
         render 'new'
       end
@@ -31,7 +31,7 @@ class BlogsController < ApplicationController
 
   def update
     if @blog.update(blog_params)
-      redirect_to blogs_path, notice: "ブログを編集しました！"
+      redirect_to blogs_path
     else
       render :edit
     end
@@ -39,7 +39,7 @@ class BlogsController < ApplicationController
 
   def destroy
     @blog.destroy
-    redirect_to blogs_path, notice:"ブログを削除しました！"
+    redirect_to blogs_path
   end
 
   def confirm
