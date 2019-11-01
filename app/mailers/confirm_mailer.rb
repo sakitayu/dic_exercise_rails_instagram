8745feb9a8186ff23f689b186f0508e25e0cbc75
sakitayu <sakitakitakita@gmail.com>
2019-11-01 17:20:17 +0900

    confirm_mailer.rbのダブルクオートを修正

--- app/mailers/confirm_mailer.rb
+++ app/mailers/confirm_mailer.rb
@@ -1,6 +1,6 @@
 class ConfirmMailer < ApplicationMailer
   def confirm_mail(confirm, current_user_info)
     @confirm = confirm
-    mail to: "current_user_info.email", subject: "投稿の確認メール"
+    mail to: current_user_info.email, subject: "投稿の確認メール"
   end
 end
