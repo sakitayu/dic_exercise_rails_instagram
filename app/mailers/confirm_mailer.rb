0ac53fafc49353842318945ea123718fc30bd2d3
sakitayu <sakitakitakita@gmail.com>
2019-11-01 16:39:49 +0900

    confirm_mailer.rb内の不要な記述を削除

--- app/mailers/confirm_mailer.rb
+++ app/mailers/confirm_mailer.rb
@@ -1,7 +1,6 @@
 class ConfirmMailer < ApplicationMailer
   def confirm_mail(confirm, current_user_info)
     @confirm = confirm
-    binding.pry
     mail to: "current_user_info.email", subject: "投稿の確認メール"
   end
 end
